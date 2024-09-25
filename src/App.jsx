6702b99cc6388cca59b0348179726da85c5bdc9c
AnuragM23 <anuragmahato006@gmail.com>
2024-09-25 14:28:36 +0530

    first commit - created the sidebar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
